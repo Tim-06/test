21c907cb58cf3f237dad7083222690bef2a7e8de
16970 <16970@24G1649>
2025-11-11 08:34:49 +0800

    加入專案檔案。

---
+++ TEST/PrimeServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using static testDemp.PrimeService;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TEST
+{
+    public class PrimeServiceTests
+    {
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void Primes_should_return_true(int n)
+        {
+            Assert.True(IsPrime(n));
+            IsPrime(n).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void NonPrimes_should_return_false(int n)
+        {
+            Assert.False(IsPrime(n));
+            IsPrime(n).Should().BeFalse();
+        }
+
+    }
+}
---
+++ testDemp/PrimeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDemp
+{
+    public static class PrimeService
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+            if (n == 2) return true;
+            if (n % 2 == 0) return false;
+            var limit = (int)Math.Sqrt(n);
+            for (int i = 3; i <= limit; i += 2)
+                if (n % i == 0) return false;
+            return true;
+        }
+    }
+}
